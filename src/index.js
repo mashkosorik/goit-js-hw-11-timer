8e2b992c795d2fcc58ae72dce52aeee413e3afcd
mashkosorik <mahandsorik@gmail.com>
2021-08-28 14:53:11 +0300

    timer

---
+++ src/index.js
@@ -0,0 +1,67 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  minutes: document.querySelector('[data-value="mins"]'),
+  seconds: document.querySelector("[data-value='secs']"),
+};
+
+class Timer {
+  constructor(startTime, stopTime) {
+    this.startTime = startTime;
+    this.stopTime = stopTime;
+    this.deltaTime = 0;
+    this.interval = '';
+  }
+
+  pad(value) {
+    return String(value).padStart(2, 0);
+  }
+
+  getDays(time) {
+    return this.pad(Math.floor(time / 1000 / 60 / 60 / 24));
+  }
+  getHours(time) {
+    return this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  }
+  getMins(time) {
+    return this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  }
+
+  getSecs(time) {
+    return this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  }
+
+  updateClockFace(time) {
+    if (time) {
+      refs.days.textContent = this.getDays(time);
+      refs.hours.textContent = this.getHours(time);
+      refs.minutes.textContent = this.getMins(time);
+      refs.seconds.textContent = this.getSecs(time);
+    } else {
+      refs.days.textContent = '00';
+      refs.hours.textContent = '00';
+      refs.minutes.textContent = '00';
+      refs.seconds.textContent = '00';
+    }
+  }
+
+  updateDate() {
+    let currentTime = Date.now();
+    this.deltaTime = Date.parse(this.stopTime) - currentTime;
+    this.updateClockFace(this.deltaTime);
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      this.updateDate();
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.interval);
+    this.updateClockFace();
+  }
+}
+
+const timer = new Timer(Date.now(), '31 dec 2021 23:59:59');
+timer.updateDate();
+timer.start();

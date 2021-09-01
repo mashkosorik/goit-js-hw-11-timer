8161cb75bf3c6f8b35012e0de6a8a84d417d201b
mashkosorik <mahandsorik@gmail.com>
2021-09-01 14:40:57 +0300

    correct timer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import { refs } from './js/refs';
-
 class Timer {
   constructor(startTime, stopTime) {
     this.startTime = startTime;
     this.stopTime = stopTime;
     this.deltaTime = 0;
     this.interval = '';
+
+    this.updateDate();
+    this.start();
   }
 
+  refs() {
+    return {
+      days: document.querySelector('[data-value="days"]'),
+      hours: document.querySelector('[data-value="hours"]'),
+      minutes: document.querySelector('[data-value="mins"]'),
+      seconds: document.querySelector("[data-value='secs']"),
+    };
+  }
   pad(value) {
     return String(value).padStart(2, 0);
   }
@@ -28,10 +37,10 @@ class Timer {
 
   updateClockFace(time) {
     if (time) {
-      refs.days.textContent = this.getDays(time);
-      refs.hours.textContent = this.getHours(time);
-      refs.minutes.textContent = this.getMins(time);
-      refs.seconds.textContent = this.getSecs(time);
+      this.refs().days.textContent = this.getDays(time);
+      this.refs().hours.textContent = this.getHours(time);
+      this.refs().minutes.textContent = this.getMins(time);
+      this.refs().seconds.textContent = this.getSecs(time);
     } else {
       refs.days.textContent = '00';
       refs.hours.textContent = '00';
@@ -57,5 +66,3 @@ class Timer {
   }
 }
 const timer = new Timer(Date.now(), '31 dec 2021 23:59:59');
-timer.updateDate();
-timer.start();
--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  minutes: document.querySelector('[data-value="mins"]'),
-  seconds: document.querySelector("[data-value='secs']"),
-};
